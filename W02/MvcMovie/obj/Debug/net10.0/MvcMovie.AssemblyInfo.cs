7da3c2754e992bc6552de6d401e28c117eeb4880
JJFRENZY <jackjayeck@icloud.com>
2026-03-11 01:03:37 -0600

    Week2.8

--- W02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ W02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccf6922a2428c83c4d77a9ef23b4e72020db3c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893fb83248b41237682e5d77c9f9306e3c14585c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
